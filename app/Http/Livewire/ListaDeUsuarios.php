d16d8adbf3d535745459d8a764d4f9e50f3c0ec8
Ana Julia <anajuliassl.67@gmail.com>
2025-01-20 20:44:20 -0400

    Aplicando paginacao, ordenacao com direcoes, limit per page

--- app/Http/Livewire/ListaDeUsuarios.php
+++ app/Http/Livewire/ListaDeUsuarios.php
@@ -5,18 +5,37 @@ namespace App\Http\Livewire;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ListaDeUsuarios extends Component
 {
-    public ?string $search = null;
+    use WithPagination;
 
+    public ?string $search = null;
+    public ?string $searchEmail = null;
+    public ?string $sortBy = null;
+    public ?string $sortDir = 'asc';
+    public ?int $perPage = 5;
     protected $listeners = [
         'user::updated' => '$refresh'
     ];
 
     protected $queryString = [
         'search' => ['except' => '', 'as' => 'q'],
+        'searchEmail' => ['except' => '', 'as' => 'eq'],
+        'perPage' => ['except' => '5', 'as' => 'p'],
+        'sortBy' => ['except' => ''],
+        'sortDir' => ['except' => 'asc'],
     ];
+
+    public function sort($coluna){
+        $this->sortDir = $this->sortDir == 'asc' ? 'desc' : 'asc';
+        $this->sortBy = $coluna;
+    }
+
+    public function updating(){
+        $this->resetPage();
+    }
 
     public function render()
     {
@@ -24,7 +43,13 @@ class ListaDeUsuarios extends Component
             'usuarios' => User::query()
                 ->when($this->search, function (Builder $query) {
                     $query->where('name', 'like', '%' . $this->search . '%');
-                })->get()
+                })
+                ->when($this->searchEmail, function (Builder $query) {
+                    $query->where('email', 'like', '%' . $this->searchEmail . '%');
+                })
+                ->when($this->sortBy, function (Builder $query) {
+                    $query->orderBy($this->sortBy, $this->sortDir);
+                })->paginate($this->perPage)
 //                ->when($this->search, fn (Builder $q) => $q->where('name', 'like', '%' . $this->search . '%'))
         ]);
     }
--- resources/views/livewire/lista-de-usuarios.blade.php
+++ resources/views/livewire/lista-de-usuarios.blade.php
@@ -2,23 +2,44 @@
     <h1>Usuarios</h1>
 
     <div class="my-4">
-        <x-text-input
-            wire:model="search"
-            placeholder="Pesquisar"
-        >
-
-        </x-text-input>
+        <x-text-input wire:model="search" placeholder="Pesquisar nome" ></x-text-input>
+        <x-text-input wire:model="searchEmail" placeholder="Pesquisar email" ></x-text-input>
+        <select class="text-black" wire:model="perPage">
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+            <option value="50">50</option>
+        </select>
     </div>
 
     <div class="relative overflow-x-auto">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" class="px-6 py-3 cursor-pointer"
+                    wire:click="sort('name')"
+                >
                     Nome
+                    @if($sortBy == 'name')
+                        @if($sortDir == 'asc')
+                            ⬇
+                        @else
+                            ⬆
+                        @endif
+                    @endif
                 </th>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" class="px-6 py-3 cursor-pointer"
+                    wire:click="sort('email')"
+                >
                     E-mail
+                    @if($sortBy == 'email')
+                        @if($sortDir == 'asc')
+                            ⬇
+                        @else
+                            ⬆
+                        @endif
+                    @endif
                 </th>
                 <th scope="col" class="px-6 py-3">
 
@@ -42,4 +63,6 @@
             </tbody>
         </table>
     </div>
+
+    {{ $usuarios->links() }}
 </div>
